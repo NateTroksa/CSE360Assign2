9e7dbcf74ece5dcac7058597320dbc7911d1908d
Nate <nate.troksa@gmail.com>
2019-02-20 11:37:14 -0700

    CSE360Assignment2
    Initial push to GitHub

---
+++ CSE360assign2/src/cse360assign2/Calculator.java
@@ -0,0 +1,51 @@
+/*
+ *  Nathaniel Troksa
+ *  ID: 1212853289
+ * CSE360 assignment 1
+ * This program is a formatted version of the provided file 
+ * The program provides the basic functions of a calculator
+ */
+
+
+package cse360assign2;
+
+public class Calculator {
+
+	private int total;
+	
+	//Constructor of the calculator class
+	public Calculator () {
+		total = 0;  // not needed - included for clarity
+	}
+	
+	//gets the total
+	public int getTotal () {
+		return 0;
+	}
+	
+	//adds to the total variable
+	public void add (int value) {
+		
+	}
+	
+	//subtracts from the total variable
+	public void subtract (int value) {
+		
+	}
+	
+	//multiplies the total variable
+	public void multiply (int value) {
+		
+	}
+	
+	
+	//divides the total variable
+	public void divide (int value) {
+		
+	}
+	
+	//returns the history of the calculators functions
+	public String getHistory () {
+		return "";
+	}
+}
