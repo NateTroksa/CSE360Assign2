eff404b4b4698d941491dee7f6bcfc3a24e9fbc9
Nate <nate.troksa@gmail.com>
2019-02-20 12:00:20 -0700

    Implementation of history variable and getHistory method

--- CSE360assign2/src/cse360assign2/Calculator.java
+++ CSE360assign2/src/cse360assign2/Calculator.java
@@ -12,40 +12,46 @@ package cse360assign2;
 public class Calculator {
 
 	private int total;
+	private String history = "";
 	
 	//Constructor of the calculator class
 	public Calculator () {
 		total = 0;  // not needed - included for clarity
+		history += "0";
 	}
 	
 	//gets the total
 	public int getTotal () {
-		return 0;
+		return total;
 	}
 	
 	//adds to the total variable
 	public void add (int value) {
 		total += value;
+		history += " + " + value;
 	}
 	
 	//subtracts from the total variable
 	public void subtract (int value) {
 		total -= value;
+		history += " - " + value;
 	}
 	
 	//multiplies the total variable
 	public void multiply (int value) {
 		total *= value;
+		history += " * " + value;
 	}
 	
 	
 	//divides the total variable
 	public void divide (int value) {
 		total /= value;
+		history += " / " + value;
 	}
 	
 	//returns the history of the calculators functions
 	public String getHistory () {
-		return "";
+		return history;
 	}
 }
